12ab3c4ff24d5b283293099dfbed1839c2a10138
WCGDOMMK\ssidappa <ssidappa@whiteclarkegroup.com>
2019-09-10 09:23:08 +0530

    adding select an multiselect list box handling code

---
+++ src/test/java/com/auto/testCase/DropDown.java
@@ -0,0 +1,60 @@
+package com.auto.testCase;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class DropDown {
+  @Test
+  public void dropdown() throws InterruptedException {
+	  System.setProperty("webdriver.chrome.driver","C:\\Java Libraries\\Drivers\\chromedriver.exe");
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("file:///C:/Users/ssidappa/Desktop/dropdown.html");
+//	  WebElement dropdown = driver.findElement(By.id("singleselect"));
+//	  Select ss  = new Select(dropdown);
+//	  ss.selectByIndex(3);
+//	  Thread.sleep(5000);
+//	  ss.selectByValue("ho");
+//	  Thread.sleep(5000);
+//	  ss.selectByVisibleText("Kia");
+//	  WebElement x = ss.getFirstSelectedOption();
+//	  System.out.println("Value which is selected : "+x.getText());
+//	  System.out.println("Single sected completed ");
+//	  
+//	  
+	  System.out.println("Multi sected Started ");
+	  WebElement msDropdown = driver.findElement(By.id("multiselect"));
+	  Select ms = new Select(msDropdown);
+	  ms.selectByIndex(4);
+	  Thread.sleep(5000);
+	  ms.selectByValue("Mahindra");
+	  Thread.sleep(1000);
+	  ms.selectByIndex(4);
+	  Thread.sleep(1000);
+	  ms.selectByVisibleText("Honda");
+	  Thread.sleep(1000);
+//	  ms.deselectAll();
+	  
+//	  Thread.sleep(5000);
+	  System.out.println("first selected option : "+ms.getFirstSelectedOption().getText());
+	  List<WebElement> selectedOptions = ms.getAllSelectedOptions();
+	  for(WebElement ob:selectedOptions) {
+		  System.out.println(ob.getText());
+	  }
+	  System.out.println("to get all lilsted options");
+	  List<WebElement> getalloptions = ms.getOptions();
+	  for (WebElement we : getalloptions) {
+		  System.out.println(we.getText());
+	  }
+	  
+	  
+	  
+
+	  
+  }
+}
